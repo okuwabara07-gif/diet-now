68d7d920dd9cf7c3302bf8d1247dae4a91114a4e
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-02 20:36:25 +0900

    feat: add GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@400;700&family=DM+Sans:wght@400;600;800&display=swap" rel="stylesheet" />
       </head>
-      <body>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-SVQXY5C3PW"></script><script dangerouslySetInnerHTML={{__html:`window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-SVQXY5C3PW');`}} /><body>
         <header className="site-header">
           <div className="header-inner">
             <a href="/" className="site-logo">
